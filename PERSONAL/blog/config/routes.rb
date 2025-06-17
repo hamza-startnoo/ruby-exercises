db6b1f74225b2a56b821376eafcea0775d4604d6
Hamza <hamza@startnoo.com>
2025-06-17 15:38:51 -0700

    completed blog example

--- PERSONAL/blog/config/routes.rb
+++ PERSONAL/blog/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
+
+  # get "/articles", to: "articles#index"
+
+  # get "/articles/:id", to: "articles#show"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
